cafdef66f888545b0f71608ba985e933baddec45
aris <72946907+EigaCerion@users.noreply.github.com>
2022-06-02 16:36:34 +0700

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,43 @@
+
+//  side nav bar 
+const sideNav = document.querySelectorAll('.sidenav');
+M.Sidenav.init(sideNav);
+//   // Or with jQuery
+//   $(document).ready(function(){
+//     $('.sidenav').sidenav();
+//   });
+
+// Akhir Side Nav Bar
+
+
+// Slider
+const slider = document.querySelectorAll('.slider');
+M.Slider.init(slider,{
+    indicators : false,
+    height : 350,
+    transition : 600,
+    interval : 3000
+
+});
+
+// Akhir Slider
+
+// Client
+const parallax = document.querySelectorAll('.parallax');
+M.Parallax.init(parallax);
+// Akhir Client
+
+// Portofolio
+const materialBox = document.querySelectorAll('.materialboxed');
+M.Materialbox.init(materialBox);
+// Akhir Portofolio
+
+// input field
+const selectForm = document.querySelectorAll('Select');
+M.FormSelect.init(selectForm);
+
+//scrollspy
+const scroll = document.querySelectorAll('.scrollspy');
+M.ScrollSpy.init(scroll,{
+    scrollOffset : 50
+});
